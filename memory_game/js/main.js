e7ac9b98ac47f58661c539ad57af612d0522a8b9
Genevieve <genschultz@gmail.com>
2018-05-16 18:19:47 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,7 +25,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
         } else {
@@ -33,8 +33,8 @@ var checkForMatch = function() {
         }  
 }
 
-var flipCard = function(cardId) {
-
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
     checkForMatch();
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
@@ -43,5 +43,15 @@ var flipCard = function(cardId) {
     
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+       var cardElement = document.createElement('img');
+       cardElement.setAttribute('src', 'images/back.png');
+       cardElement.setAttribute('data-id', i);
+       document.getElementsByTagName('')[].addEventListener('click', cardElement); 
+       flipCard();
+       cardElement.appendChild('#game-board');
+    }
+}
+
+createBoard();
